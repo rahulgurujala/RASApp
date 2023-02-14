aabff927842e7640dec1fc07f86121f64f844b86
rahulgurujala <isaacnewtonrahul@gmail.com>
2023-02-15 02:36:56 +0530

    fixed tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,6 @@
-def test_user_device_association(UserFactory, DeviceFactory):
-    user = UserFactory()
-    device = DeviceFactory(user=user)
+def test_user_device_association(user_factory, device_factory):
+    user = user_factory()
+    device = device_factory(user=user)
 
     # Your test assertions go here
     assert user.devices == [device]
